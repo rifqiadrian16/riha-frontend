dc7897c5fe7a49a776279c03d0280fb137469a54
Muhamad Rifqi Adrian <rifqiadrian0416@gmail.com>
2026-04-14 08:59:17 +0700

    bismillah

--- src/router/index.js
+++ src/router/index.js
@@ -1,81 +1,66 @@
 import { createRouter, createWebHistory } from "vue-router";
 
-// Import View
-import LandingView from "../views/LandingView.vue"; // <--- Import Landing View
-import LoginView from "../views/auth/LoginView.vue";
-import RegisterView from "../views/auth/RegisterView.vue";
-import HomeView from "../views/dashboard/HomeView.vue";
-import AntreanView from "../views/dashboard/AntreanView.vue";
-import RSTerdekatView from "../views/dashboard/RSTerdekatView.vue";
-import InboxView from "../views/dashboard/InboxView.vue";
-import NotifikasiView from "../views/dashboard/NotifikasiView.vue";
-import PengaturanView from "../views/dashboard/PengaturanView.vue";
-import AdminQueueView from "../views/dashboard/AdminQueueView.vue";
-import ActivationView from "../views/auth/ActivationView.vue";
-import ForgotPasswordView from "../views/auth/ForgotPasswordView.vue";
-import ResetPasswordView from "../views/auth/ResetPasswordView.vue";
-
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: "/",
       name: "landing",
-      component: LandingView, // <--- Ubah ini! Dulu redirect ke login, sekarang ke LandingView
+      component: () => import("../views/LandingView.vue"), 
     },
     {
       path: "/login",
       name: "login",
-      component: LoginView,
+      component: () => import("../views/auth/LoginView.vue"),
     },
     {
       path: "/register",
       name: "register",
-      component: RegisterView,
+      component: () => import("../views/auth/RegisterView.vue"),
     },
     {
       path: "/dashboard",
       name: "dashboard",
-      component: HomeView,
+      component: () => import("../views/dashboard/HomeView.vue"),
     },
     {
-      path: "/antrean", // <-- Link url
+      path: "/antrean", 
       name: "antrean",
-      component: AntreanView,
+      component: () => import("../views/dashboard/AntreanView.vue"),
     },
     {
       path: "/rs-terdekat",
       name: "rs-terdekat",
-      component: RSTerdekatView,
+      component: () => import("../views/dashboard/RSTerdekatView.vue"),
     },
     {
       path: "/inbox",
       name: "inbox",
-      component: InboxView,
+      component: () => import("../views/dashboard/InboxView.vue"),
     },
     {
       path: "/notifikasi",
       name: "notifikasi",
-      component: NotifikasiView,
+      component: () => import("../views/dashboard/NotifikasiView.vue"),
     },
     {
       path: "/pengaturan",
       name: "pengaturan",
-      component: PengaturanView,
+      component: () => import("../views/dashboard/PengaturanView.vue"),
     },
     {
       path: "/admin/antrean",
       name: "admin-antrean",
-      component: AdminQueueView,
+      component: () => import("../views/dashboard/AdminQueueView.vue"),
       meta: { requiresAuth: true },
     },
     {
       path: "/activate-account",
       name: "activate-account",
-      component: ActivationView,
+      component: () => import("../views/auth/ActivationView.vue"),
     },
-    { path: "/forgot-password", component: ForgotPasswordView },
-    { path: "/reset-password", component: ResetPasswordView },
+    { path: "/forgot-password", component: () => import("../views/auth/ForgotPasswordView.vue") },
+    { path: "/reset-password", component: () => import("../views/auth/ResetPasswordView.vue") },
   ],
 });
 
